da1bf71502f94b2e9c8699d2f37c9174aba58d20
Meng Zhao <jpja19981224@gmail.com>
2021-03-18 03:25:09 +0800

    build header for mobile version

---
+++ script/productDetail.js
@@ -0,0 +1,22 @@
+/*function changePrice(){
+	size = document.querySelector("option")
+	if size.value == "15ml"{
+		document.querySelector("div.nameAndPrice>p").innerHTML = "CAD $25.00"
+	} else if size.value == "50ml"{
+		document.querySelector("div.nameAndPrice>p").innerHTML = "CAD $50.00"
+	} else if size.value == "100ml"{
+		document.querySelector("div.nameAndPrice>p").innerHTML = "CAD $100.00"
+	} else if size.value == "500ml"{
+		document.querySelector("div.nameAndPrice>p").innerHTML = "CAD $150.00"
+	}
+}*/
+
+show = false;
+function showMenu(){
+	if (show == false){
+		document.querySelector("#dropDown").style.display="block";
+	} else{
+		document.querySelector("#dropDown").style.display="none";
+	}
+	show = !show;
+}
