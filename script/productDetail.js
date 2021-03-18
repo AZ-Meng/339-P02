7fe8064b9f3ee2fd9479d974fc6d73bccb4d8112
Meng Zhao <jpja19981224@gmail.com>
2021-03-18 13:31:52 +0800

    improve the mobile version header

--- script/productDetail.js
+++ script/productDetail.js
@@ -1,13 +1,14 @@
-/*function changePrice(){
-	size = document.querySelector("option")
-	if size.value == "15ml"{
+function changePrice(){
+
+	size = document.querySelector("#sizes");
+	if (size.value == "15ml"){
 		document.querySelector("div.nameAndPrice>p").innerHTML = "CAD $25.00"
-	} else if size.value == "50ml"{
+	} else if (size.value == "50ml"){
 		document.querySelector("div.nameAndPrice>p").innerHTML = "CAD $50.00"
-	} else if size.value == "100ml"{
+	} else if (size.value == "100ml"){
 		document.querySelector("div.nameAndPrice>p").innerHTML = "CAD $100.00"
-	} else if size.value == "500ml"{
+	} else if (size.value == "500ml"){
 		document.querySelector("div.nameAndPrice>p").innerHTML = "CAD $150.00"
 	}
-}*/
+}
 
